eb7b4f5db7a3e1f9184854f5dbe23842d43722f2
Philippe Paradis <philippe.paradis@gmail.com>
2017-02-20 02:20:46 -0500

    Add a few more image generator models

--- known-models/download-models.sh
+++ known-models/download-models.sh
@@ -16,7 +16,14 @@ git clone https://github.com/kelvinxu/arctic-captions
 popd
 
 pushd image-generator
-git clone https://github.com/hanzhanggit/StackGAN.git
+# Generative Adversarial Text-to-Image Synthesis
 git clone https://github.com/reedscot/icml2016 reedscot-icml2016
+# Learning Deep Representations of Fine-grained Visual Descriptions
 git clone https://github.com/reedscot/cvpr2016 reedscot-cvpr2016
+# StackGAN: Text to Photo-realistic Image Synthesis with Stacked Generative Adversarial Networks
+git clone https://github.com/hanzhanggit/StackGAN.git
+# Conditional generative adversarial nets
+git clone https://github.com/zhangqianhui/Conditional-Gans
+# Generating images from captions with attention
+git clone https://github.com/emansim/text2image
 popd
