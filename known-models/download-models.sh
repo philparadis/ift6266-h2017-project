7f3fcdac1e681b08b613606aca897ff6e45a00ba
Philippe Paradis <philippe.paradis@gmail.com>
2017-03-01 14:31:31 -0500

    Small update to script and deleted temporary file

--- known-models/download-models.sh
+++ known-models/download-models.sh
@@ -3,19 +3,21 @@ set -o errexit off
 set -o verbose on
 
 mkdir -p misc
-mkdir -p caption-generator
-mkdir -p image-generator
-
 pushd misc
 git clone https://github.com/tensorflow/models.git tensorflow-models
 popd
 
+mkdir -p caption-generator
 pushd caption-generator
 ln -s ../misc/tensorflow-models/im2txt/
 git clone https://github.com/kelvinxu/arctic-captions
 popd
 
+
+mkdir -p image-generator
 pushd image-generator
+# "Semantic Image Inpainting with Perceptual and Contextual Losses"
+git clone https://github.com/bamos/dcgan-completion.tensorflow
 # Generative Adversarial Text-to-Image Synthesis
 git clone https://github.com/reedscot/icml2016 reedscot-icml2016
 # Learning Deep Representations of Fine-grained Visual Descriptions
